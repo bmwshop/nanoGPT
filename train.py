07c780b7c8fd170c562a53b562c3053dd0f34c5e
Dima Rekesh <bmwshop@gmail.com>
2024-10-11 14:43:04 -0700

    flash and wpe flags

--- train.py
+++ train.py
@@ -149,7 +149,7 @@ if os.path.exists(meta_path):
 
 # model init
 model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
-                  wpe=wpe,flash=flash
+                  wpe=wpe,flash=flash,
                   bias=bias, vocab_size=None, dropout=dropout) # start with model_args from command line
 if init_from == 'scratch':
     # init a new model from scratch

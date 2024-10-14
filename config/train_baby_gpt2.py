92f488a5ac3ac59c1f9f41afcf4a42b9048d54c2
Dima Rekesh <bmwshop@gmail.com>
2024-10-13 20:28:28 -0700

    baby gpt2

--- config/train_baby_gpt2.py
+++ config/train_baby_gpt2.py
@@ -1,15 +1,17 @@
 
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
-wandb_log = True
+out_dir = 'out-baby-gpt'
+
+wandb_log = False
 wandb_project = 'owt'
 wandb_run_name='gpt2-baby'
 
 # these make the total batch size be ~0.5M
-# 12 batch size * 1024 block size * 10 gradaccum * 4 GPUs = 491,520
-batch_size = 12
+# 64 batch size * 1024 block size * 4 gradaccum  = 491,520
+batch_size = 64
 block_size = 1024
-gradient_accumulation_steps = 10
+gradient_accumulation_steps = 8
 
 # baby GPT model :)
 n_layer = 6

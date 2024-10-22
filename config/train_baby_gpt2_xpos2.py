8c1070ad2705d2387ad6899a1a00ea63d54f1345
Dima Rekesh <bmwshop@gmail.com>
2024-10-21 17:06:24 -0700

    baby xpos2 config

---
+++ config/train_baby_gpt2_xpos2.py
@@ -0,0 +1,50 @@
+
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+out_dir = 'out-baby-gpt'
+
+wandb_log = False
+wandb_project = 'owt'
+wandb_run_name='gpt2-baby'
+
+# these make the total batch size be ~0.5M
+# 64 batch size * 1024 block size * 4 gradaccum  = 491,520
+batch_size = 64
+block_size = 1024
+gradient_accumulation_steps = 8
+
+# baby GPT model :)
+n_layer = 6
+n_head = 6
+n_embd = 384
+# dropout = 0.2
+
+learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+# max_iters = 5000
+# lr_decay_iters = 5000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100 # not super necessary potentially
+
+
+# this makes total number of tokens be 300B
+max_iters = 600000
+lr_decay_iters = 600000
+
+# eval stuff
+eval_interval = 1000
+eval_iters = 200
+log_interval = 10
+
+# weight decay
+weight_decay = 1e-1
+
+pe = 'xpos2'
+xpos2_decay_base = 2.0
+xpos2_decay_angle = 1.570796327 # math.pi / 2
+xpos2_adaptive = True
+rope_base = 10000
+flash = True
+
+softmax_log_k = 0.0

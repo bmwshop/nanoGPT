1b2bdd676b0d31627f58d71953fa0582111c7da3
Dima Rekesh <bmwshop@gmail.com>
2024-11-20 20:52:24 -0800

    bug

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-from rotary_position_embedding import RotaryEmbedding, apply_rotary_pos_emb
+from rotary_position_embedding import RotaryEmbedding
 from xpos2_position_embedding import Xpos2Embedding, apply_xpos2_emb
 from alibi_relative_position_embedding import build_slopes, build_relative_position
 try:

9ae590ab042cfc00a53d89641665db3d6684484a
Dima Rekesh <bmwshop@gmail.com>
2024-12-13 08:53:08 -0300

    handling none type vars

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ rope_base = 10000
 rope_percentage = 1.0
 rope_wavelengths = None
 # -----------------------------------------------------------------------------
-config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
+config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str, type(None)))]
 exec(open('configurator.py').read()) # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
 # -----------------------------------------------------------------------------
